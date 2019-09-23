5e4c0a8b42da3098cc904e633c6748f5884bc009
Taras Grynik <grynik25.03@gmail.com>
2019-09-23 17:47:23 +0300

    added task-1 module 3

--- module-3/task-1.js
+++ module-3/task-1.js
@@ -1,19 +1,37 @@
 /* eslint-disable no-restricted-syntax */
+
+'use strick';
+
 const user = {
   name: 'Mango',
   age: 20,
   hobby: 'html',
   premium: true,
+  addValue(key, value) {
+    this[key] = value;
+  },
+  changeValue(key, value) {
+    this[key] = value;
+  },
 };
 
-user.mood = 'happy';
-user.hobby = 'javascript';
-user.premium = 'false';
+console.log(user.addValue('mood', 'happy'));
+console.log(user.changeValue('hobby', 'javascript'));
+console.log(user.changeValue('premium', false));
 
-const keys = Object.keys(user);
+/* add property via dot notation */
+// user.mood = 'happy';
+// user.hobby = 'javascript';
+// user.premium = 'false';
 
-let value = '';
-for (const key of keys) {
-  value += `${key}: ${user[key]} \n`;
+function showResult(obj) {
+  const keys = Object.keys(obj);
+  let value = '';
+
+  for (const key of keys) {
+    value += `${key}: ${obj[key]} \n`;
+  }
+  return value;
 }
-console.log(value);
+
+console.log(showResult(user));
