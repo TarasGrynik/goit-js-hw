d089ddf3a7190bdbd867f5041bfe33a82dcc4a6c
Taras Grynik <grynik25.03@gmail.com>
2019-09-25 13:42:57 +0300

    fixed task-1 module3

--- module-3/task-1.js
+++ module-3/task-1.js
@@ -7,31 +7,19 @@ const user = {
   age: 20,
   hobby: 'html',
   premium: true,
-  addValue(key, value) {
-    this[key] = value;
-  },
-  changeValue(key, value) {
-    this[key] = value;
-  },
 };
 
-console.log(user.addValue('mood', 'happy'));
-console.log(user.changeValue('hobby', 'javascript'));
-console.log(user.changeValue('premium', false));
-
 /* add property via dot notation */
-// user.mood = 'happy';
-// user.hobby = 'javascript';
-// user.premium = 'false';
+user.mood = 'happy';
+user.hobby = 'javascript';
+user.premium = 'false';
 
 function showObjProp(obj) {
   const keys = Object.keys(obj);
-  let value = '';
 
   for (const key of keys) {
-    value += `${key}: ${obj[key]} \n`;
+    console.log(`${key}: ${obj[key]}`);
   }
-  return value;
 }
 
 console.log(showObjProp(user));
