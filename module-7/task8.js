17a44f226b171c816ae1420153125b28cf85cb8f
Taras Grynik <grynik25.03@gmail.com>
2019-10-30 21:42:15 +0200

    added module 7

--- module-7/task8.js
+++ module-7/task8.js
@@ -5,6 +5,12 @@ const refs = {
   box: document.querySelector('#boxes'),
 };
 
+const elemSize = {
+  width: 30,
+  height: 30,
+  sizeStep: 10,
+};
+
 
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const randomByte = () => randomNumber(0, 255);
@@ -13,10 +19,15 @@ const randomCssRgba = () => `rgba(${[randomByte(), randomByte(), randomByte(), r
 
 function createBoxes(amount) {
   const itemList = [];
+  let additionalBlockWidth = 0;
+
   for (let i = 0; i < amount; i += 1) {
     const newElem = document.createElement('div');
     newElem.textContent = i;
     newElem.style.backgroundColor = randomCssRgba();
+    newElem.style.width = `${elemSize.width + additionalBlockWidth}px`;
+    newElem.style.height = `${elemSize.height + additionalBlockWidth}px`;
+    additionalBlockWidth += elemSize.sizeStep;
     itemList.push(newElem);
   }
 
